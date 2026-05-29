0d8149668138252e07525cd7a98888c5ffde6e98
Justin <ju3tin@hotmail.co.uk>
2026-05-29 13:59:21 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,16 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-
   images: {
     unoptimized: true,
   },
-
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    })
-
-    return config
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
   },
 }
 
