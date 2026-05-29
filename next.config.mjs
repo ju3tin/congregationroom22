feb67635316c3f21400c796c7e85000602ea6a7a
Justin <ju3tin@hotmail.co.uk>
2026-05-29 13:45:12 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,24 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
+
   turbopack: {
     rules: {
       "*.svg": {
         loaders: ["@svgr/webpack"],
         as: "*.js",
       },
+
+      "*.less": {
+        loaders: ["less-loader"],
+        as: "*.css",
+      },
     },
   },
 }
 
 export default nextConfig
-
