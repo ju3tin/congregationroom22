a6d5b729d3eae75b8fcca4c2abb8691c2091d423
Justin <ju3tin@hotmail.co.uk>
2026-05-30 00:25:27 +0100

    Update route.ts

--- app/api/timeline/route.ts
+++ app/api/timeline/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import connectDB from '@/lib/db';
 import TimelineEvent from '@/models/TimelineEvent';
-
+ 
 export async function GET() {
   await connectDB();
 
