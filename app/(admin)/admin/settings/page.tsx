3940c8e84f5f1ebc415743406d522d7af9134a0f
Justin <ju3tin@hotmail.co.uk>
2026-06-02 14:47:15 +0100

    Add client directive to settings page

--- app/(admin)/admin/settings/page.tsx
+++ app/(admin)/admin/settings/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import dbConnect from "@/lib/db";
 import { SiteSettings } from "@/models";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
