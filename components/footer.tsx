7f1fafe32ee46f908028176ec6f6083ddeee86bd
Justin <ju3tin@hotmail.co.uk>
2026-06-03 15:42:26 +0100

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -85,7 +85,7 @@ export function Footer() {
 
         <div className="border-t border-border mt-8 pt-8 text-center text-sm text-muted-foreground">
           <p>&copy; {new Date().getFullYear()} Congregation Room 22. All rights reserved.</p>
-          <p>Built by <Link href="https://ju3tin.github.io">Justin Grierson</Link></p>
+          <p>Built by <Link href="https://ju3tin.github.io">Justin Grierson</Link><br /></p>
         </div>
       </div>
     </footer>
