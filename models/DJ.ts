42222e94e6ba7f92799023aba85ed0bdfdc3deee
Justin <ju3tin@hotmail.co.uk>
2026-06-03 09:18:16 +0100

    Create DJ.ts

---
+++ models/DJ.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Document, Model, Types } from "mongoose"
+
+export interface IDJ extends Document {
+  _id: Types.ObjectId
+  name: string
+  slug: string
+  genre: string
+  bio: string
+  image: string
+  socialLinks: {
+    instagram?: string
+    soundcloud?: string
+    twitter?: string
+  }
+  createdAt: Date
+  updatedAt: Date
+}
+
+const DJSchema = new Schema<IDJ>(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    genre: { type: String, required: true },
+    bio: { type: String, required: true },
+    image: { type: String, required: true },
+    socialLinks: {
+      instagram: String,
+      soundcloud: String,
+      twitter: String,
+    },
+  },
+  { timestamps: true }
+)
+
+DJSchema.index({ slug: 1 })
+DJSchema.index({ genre: 1 })
+
+const DJ: Model<IDJ> =
+  mongoose.models.DJ || mongoose.model<IDJ>("DJ", DJSchema)
+
+export default DJ
