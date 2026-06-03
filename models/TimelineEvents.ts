7ff273a532c2c70294ce96d9b4c7f494ed954fd3
Justin <ju3tin@hotmail.co.uk>
2026-06-03 23:00:16 +0100

    Rename model from 'TimelineEvent' to 'TimelineEvents'

---
+++ models/TimelineEvents.ts
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const TimelineEventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+
+    description: {
+      type: String,
+      required: true
+    },
+
+    startDate: {
+      year: { type: Number, required: true },
+      month: Number,
+      day: Number
+    },
+
+    endDate: {
+      year: Number,
+      month: Number,
+      day: Number
+    },
+
+    category: String,
+
+    tags: [String],
+
+    media: {
+      url: String,
+      caption: String,
+      credit: String,
+      thumbnail: String
+    },
+
+    location: {
+      name: String,
+      latitude: Number,
+      longitude: Number
+    },
+
+    sources: [
+      {
+        title: String,
+        url: String
+      }
+    ],
+
+    featured: {
+      type: Boolean,
+      default: false
+    },
+
+    sortOrder: {
+      type: Number,
+      default: 0
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.model("TimelineEvents", TimelineEventSchema);
