8cdd80c59d3e7342edcb9063dd94c279400fa5b7
Justin <ju3tin@hotmail.co.uk>
2026-06-08 08:35:13 +0100

    Update event-edit-form.tsx

--- app/(admin)/admin/events/[id]/edit1/event-edit-form.tsx
+++ app/(admin)/admin/events/[id]/edit1/event-edit-form.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 import { ArrowLeft, Plus, Trash2 } from "lucide-react"
 import { toast } from "sonner"
 
-import { createEvent } from "../actions"
+import { createEvent } from "./actions"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
