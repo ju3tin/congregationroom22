484b9abafaee6ccc5e07d821b9c207a889a6ea7e
Justin <ju3tin@hotmail.co.uk>
2026-06-16 12:06:37 +0100

    Implement CheckoutForm component for donations

---
+++ components/CheckoutForm.tsx
@@ -0,0 +1,17 @@
+// Partial of app/components/CheckoutForm.tsx
+"use client";
+
+import { createCheckoutSession } from "@/actions/stripe";
+
+const formAction = async (data: FormData): Promise<void> => {
+  const { url } = await createCheckoutSession(data);
+  window.location.assign(url as string);
+};
+
+return (
+  <form action={formAction}>
+    <input type="hidden" name="uiMode" value="hosted" />
+    {/* donation input */}
+    <button type="submit">Donate</button>
+  </form>
+);
