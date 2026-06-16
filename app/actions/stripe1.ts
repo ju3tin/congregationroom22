526b6211d25524a00705f5227879b3a8c167d984
Justin <ju3tin@hotmail.co.uk>
2026-06-16 12:05:09 +0100

    Create stripe1.ts

---
+++ app/actions/stripe1.ts
@@ -0,0 +1,37 @@
+// Partial of app/actions/stripe.ts
+"use server";
+
+import type { Stripe } from "stripe";
+import { stripe } from "@/lib/stripe";
+import { formatAmountForStripe } from "@/utils/stripe-helpers";
+import { CURRENCY } from "@/config";
+
+export async function createCheckoutSession(
+  data: FormData,
+): Promise<{ client_secret: string | null; url: string | null }> {
+  const checkoutSession: Stripe.Checkout.Session =
+    await stripe.checkout.sessions.create({
+      mode: "payment",
+      submit_type: "donate",
+      line_items: [
+        {
+          quantity: 1,
+          price_data: {
+            currency: CURRENCY,
+            product_data: { name: "Custom amount donation" },
+            unit_amount: formatAmountForStripe(
+              Number(data.get("customDonation") as string),
+              CURRENCY,
+            ),
+          },
+        },
+      ],
+      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/donate-with-checkout/result?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/donate-with-checkout`,
+    });
+
+  return {
+    client_secret: checkoutSession.client_secret,
+    url: checkoutSession.url,
+  };
+}
