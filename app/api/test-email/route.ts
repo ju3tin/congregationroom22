9962d425c775b7f722441424d64d2a93386fee0d
Justin <ju3tin@hotmail.co.uk>
2026-06-17 10:16:38 +0100

    Update route.ts

--- app/api/test-email/route.ts
+++ app/api/test-email/route.ts
@@ -42,7 +42,7 @@ export async function POST(req: NextRequest) {
           <br/>
 
           <img
-            src="https://congregationroom22.vercel.app/api/tickets/${qrCode}/qr"
+            src="https://congregationroom22.vercel.app/api/tickets/${ticketCode}/qr"
             width="250"
             height="250"
           />
