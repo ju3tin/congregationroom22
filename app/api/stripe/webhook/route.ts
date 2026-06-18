b65c2a532e96054cb46f4843506f77f7e53859bf
Justin <ju3tin@hotmail.co.uk>
2026-06-18 23:27:45 +0100

    Update mailer import from mailer3 to mailer2

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -6,7 +6,7 @@ import { v4 as uuidv4 } from "uuid";
 import dbConnect from "@/lib/db";
 import Order from "@/models/Order";
 import Ticket from "@/models/Ticket";
-import { sendTicketEmail } from "@/lib/mailer3";
+import { sendTicketEmail } from "@/lib/mailer2";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2024-06-20",
